529b742805a96114c80d439358c9953564c8f2ef
hoorewea <alexis.hoorewege@utt.fr>
2021-11-30 00:17:08 +0100

    group.js

---
+++ objet.js
@@ -0,0 +1,191 @@
+var question = function(){
+	this.num = 0;
+	this.type =  [];
+	this.titre =  [];
+	this.question =  [];
+    this.reponse=[];
+    this.instruction=[];
+    this.localisation=[];
+}
+
+//Fonction qui mets le numéro de la question dans l'objet objet.num
+//le paramère data est le parsedPOI
+question.prototype.triNum=function(data){
+
+    //on définit le séparateur
+    var separator2 = (': ');
+    //On sépare la question en deux si ça marche
+    //--> le premier membre est le numéro et deuxième est le reste
+	data = data.split(separator2);
+    //On filtre: n retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator2));
+    return data[0];
+
+}
+
+question.prototype.triTitre1=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('<b>');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[1];
+
+}
+
+question.prototype.triTitre2=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('</b>');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[0];
+
+}
+
+question.prototype.triLocalisation=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('::');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[1];
+
+}
+
+question.prototype.triType1=function(data){
+
+    //on définit le séparateur
+    var separator3 = (': ');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[1];
+
+}
+
+question.prototype.triType2=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('::');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[0];
+
+}
+
+question.prototype.triInstruction1=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('::');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[2];
+
+}
+
+question.prototype.triInstruction2=function(data){
+
+    //on définit le séparateur
+    var separator3 = ('<');
+    //On sépare la question     .split(/,| /) 
+    //--> le deuxième membre est le titre 
+	data = data.split(separator3);
+    //On filtre: on retire tout ce qui est le séparateur
+	data = data.filter((val, idx) => !val.match(separator3));
+  
+   //On return le titre
+    return data[0];
+
+}
+
+question.prototype.tri = function(data){
+
+    //Avoir le numéro
+	var numero = this.triNum(data);
+	console.log("Normalement il y a 1!!!"+numero);
+	this.num=numero;
+    console.log(this.num);
+
+    //Avoir le titre
+    var titre = this.triTitre1(data);
+
+    var titreFinale = this.triTitre2(titre);
+	console.log("Normalement il y a The science of happiness!!!"+titreFinale);
+	this.titre=titre;
+
+    //Avoir la localisation
+    var localisation=this.triLocalisation(data);
+    console.log("Normalement il y a EM U4 p32 Review 1 MultiChoice!!!"+localisation);
+	this.localisation=localisation;
+
+    //Avoir l'instruction
+    var instruction=this.triInstruction1(data);
+
+    var instructionFinale=this.triInstruction2(instruction);
+    console.log("Normalement il y a Read the text below and decide which answer best fits each gap. There is an example at the beginning.!!!"+ instructionFinale);
+	this.instruction=instructionFinale;
+
+    //Avoir le type
+    var type=this.triType1(data);
+
+    var typeFinale=this.triType2(type);
+    console.log("Normalement il y a MultiChoice!!!"+ typeFinale);
+	this.type=instructionFinale;
+
+    if(this.type=='Multiple Choice'){
+
+        var phrase1=this.triQuestionMC1(data);
+        var phrase1Intermediaire=this.triQuestionMC2(phrase1);
+        phrase1Intermediaire=phrase1Intermediaire+"<insert answer>";
+        var phrase1PreFinale=this.triQuestionMC3(phrase1Intermediaire);
+        var phrase1Finale=this.triQuestionMC4(phrase1PreFinale);
+
+        console.log("People are attracted to attempting extreme activities for a <insert answer> of reasons"+ phrase1Finale);
+	    this.question[0]=phrase1Finale;
+
+
+
+    }
+    else if(this.type=='Open Cloze'){
+
+    }
+    else if(this.type=='Word formation'){
+
+    }
+    else{//Key word transformation
+
+    }
+
+}
+
+module.exports = question;
