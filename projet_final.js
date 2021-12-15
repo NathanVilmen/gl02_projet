44fa944228584ee5e23f04d108447c058bd39cb5
MaelyTan <tanmaely@live.fr>
2021-12-15 16:47:45 +0100

    Lecture de fichier inséré dans une fonction, optimisation du code - Closes #9

--- projet_final.js
+++ projet_final.js
@@ -12,6 +12,29 @@ const path = require("path");
 const vg = require('vega');
 const vegalite = require('vega-lite');
 
+/**
+ * Permet de récupérer tous les fichiers du dossier SujetB_data
+ * @returns myMap - une map qui contient tous les dossiers et leur ID (exemple : 0, EM-U4-p32_33-Review.gift)
+ */
+let getDataFromAllFiles = function(){
+    //On définit la Map pour pouvoir associer le numéro de fichier à un path
+    const myMap = new Map();
+    const dir = './SujetB_data';
+
+    let fichiers = fs.readdirSync(dir);
+    let i = 0;
+
+    fichiers.forEach(fichier => {
+        //On ajoute chaque fichier qui termine par .gift dans la constante myMap
+        if(fichierValide = fichier.match(/.gift$/)) {
+            myMap.set(i, dir+"/"+fichierValide[0]);
+            i++;
+        }
+    })
+
+    return myMap;
+}
+
 program
     // Sert à séparer le fichier par question
     //.version('vpf-parser-cli')
@@ -117,55 +140,7 @@ program
     .argument("<nomF>", "C'est le nom du fichier d'examen souhaité")
     .action(({args,logger}) => {
 
-        //On définit la Map pour pouvoir associer le numéro de fichier à un path
-        const myMap = new Map();
-        myMap.set(0, "./SujetB_data/EM-U4-p32_33-Review.gift");
-        myMap.set(1, "./SujetB_data/EM-U5-p34-Gra-Expressions_of_quantity.gift");
-        myMap.set(2, "./SujetB_data/EM-U5-p34-Voc.gift");
-        myMap.set(3, "./SujetB_data/EM-U5-p35-Gra-Subject_verb_agreement.gift");
-        myMap.set(4, "./SujetB_data/EM-U5-p36_37-Reading.gift");
-        myMap.set(5, "./SujetB_data/EM-U5-p38-Passive.gift");
-        myMap.set(6, "./SujetB_data/EM-U6-p46_47-4.gift");
-        myMap.set(7, "./SujetB_data/EM-U42-Ultimate.gift");
-        myMap.set(8, "./SujetB_data/U1-p7-Adverbs.gift");
-        myMap.set(9, "./SujetB_data/U1-p8_9-Reading-Coachella.gift");
-        myMap.set(10, "./SujetB_data/U1-p10-Gra-Present_tenses_habits.gift");
-        myMap.set(11, "./SujetB_data/U2-p22-Gra-Ing_or_inf.gift");
-        myMap.set(12, "./SujetB_data/U3-p30-Reading.gift");
-        myMap.set(13, "./SujetB_data/U3-p31-Gra-ed_adjectives_prepositions.gift");
-        myMap.set(14, "./SujetB_data/U3-p32-Gra-Present_perfect_simple_vs_continuous.gift");
-        myMap.set(15, "./SujetB_data/U3-p32-GR-Present_perfect_vs_past_simple.gift");
-        myMap.set(16, "./SujetB_data/U3-p33-Gra-As_like.gift");
-        myMap.set(17, "./SujetB_data/U3-p33-UoE-Hygge.gift");
-        myMap.set(18, "./SujetB_data/U4-p39-Reading-xmen.gift");
-        myMap.set(19, "./SujetB_data/U4-p42_43-Listening.gift");
-        myMap.set(20, "./SujetB_data/U5-p49-GR1-Expressions_of_quantity.gift");
-        myMap.set(21, "./SujetB_data/U5-p49-Subject_verb_agreement.gift");
-        myMap.set(22, "./SujetB_data/U5-p52-Reading-The_death_of_cooking.gift");
-        myMap.set(23, "./SujetB_data/U5-p54-6-Passive.gift");
-        myMap.set(24, "./SujetB_data/U5-p54-GR4-Passive-reporting.gift");
-        myMap.set(25, "./SujetB_data/U5-p57-Review.gift");
-        myMap.set(26, "./SujetB_data/U6-p59-Vocabulary.gift");
-        myMap.set(27, "./SujetB_data/U6-p60-Listening.gift");
-        myMap.set(28, "./SujetB_data/U6-p61-5-Future-forms.gift");
-        myMap.set(29, "./SujetB_data/U6-p61-GR-Future_forms.gift");
-        myMap.set(30, "./SujetB_data/U6-p62_63-Reading.gift");
-        myMap.set(31, "./SujetB_data/U6-p64-Future-perfect-&-continuous.gift");
-        myMap.set(32, "./SujetB_data/U6-p65-Voc-Expressions_with_get.gift");
-        myMap.set(33, "./SujetB_data/U6-p67-Review.gift");
-        myMap.set(34, "./SujetB_data/U6-p68_69-ProgressTest2.gift");
-        myMap.set(35, "./SujetB_data/U7-p76_77-So,such,too,enough.gift");
-        myMap.set(36, "./SujetB_data/U7-p76-Relative_clauses.gift");
-        myMap.set(37, "./SujetB_data/U7-p77-6.gift");
-        myMap.set(38, "./SujetB_data/U7-p77-It is,there is.gift");
-        myMap.set(39, "./SujetB_data/U7-p79-Review-3.gift");
-        myMap.set(40, "./SujetB_data/U8-p84-Voc-Linking_words.gift");
-        myMap.set(41, "./SujetB_data/U9-p94-Listening.gift");
-        myMap.set(42, "./SujetB_data/U9-p95-Third_cond-4.gift");
-        myMap.set(43, "./SujetB_data/U10-p106-Reading.gift");
-        myMap.set(44, "./SujetB_data/U11-p114-Mixed_conditionals.gift");
-        myMap.set(45, "./SujetB_data/U4-p47-Review.gift");
-        myMap.set(46, "./SujetB_data/U5-p50-Use_of_English.gift");
+        const myMap = getDataFromAllFiles();
 
         var numF=args.numF;
         var numEnonce=args.numEnonce;
@@ -232,55 +207,9 @@ program
     //Spec3
     .command('viewer', 'Pour rechercher et visualiser une question')
     .action(({logger}) => {
-        console.log("C'est bon")
-        const myMap = new Map();
-        myMap.set(0, "./SujetB_data/EM-U4-p32_33-Review.gift");
-        myMap.set(1, "./SujetB_data/EM-U5-p34-Gra-Expressions_of_quantity.gift");
-        myMap.set(2, "./SujetB_data/EM-U5-p34-Voc.gift");
-        myMap.set(3, "./SujetB_data/EM-U5-p35-Gra-Subject_verb_agreement.gift");
-        myMap.set(4, "./SujetB_data/EM-U5-p36_37-Reading.gift");
-        myMap.set(5, "./SujetB_data/EM-U5-p38-Passive.gift");
-        myMap.set(6, "./SujetB_data/EM-U6-p46_47-4.gift");
-        myMap.set(7, "./SujetB_data/EM-U42-Ultimate.gift");
-        myMap.set(8, "./SujetB_data/U1-p7-Adverbs.gift");
-        myMap.set(9, "./SujetB_data/U1-p8_9-Reading-Coachella.gift");
-        myMap.set(10, "./SujetB_data/U1-p10-Gra-Present_tenses_habits.gift");
-        myMap.set(11, "./SujetB_data/U2-p22-Gra-Ing_or_inf.gift");
-        myMap.set(12, "./SujetB_data/U3-p30-Reading.gift");
-        myMap.set(13, "./SujetB_data/U3-p31-Gra-ed_adjectives_prepositions.gift");
-        myMap.set(14, "./SujetB_data/U3-p32-Gra-Present_perfect_simple_vs_continuous.gift");
-        myMap.set(15, "./SujetB_data/U3-p32-GR-Present_perfect_vs_past_simple.gift");
-        myMap.set(16, "./SujetB_data/U3-p33-Gra-As_like.gift");
-        myMap.set(17, "./SujetB_data/U3-p33-UoE-Hygge.gift");
-        myMap.set(18, "./SujetB_data/U4-p39-Reading-xmen.gift");
-        myMap.set(19, "./SujetB_data/U4-p42_43-Listening.gift");
-        myMap.set(20, "./SujetB_data/U5-p49-GR1-Expressions_of_quantity.gift");
-        myMap.set(21, "./SujetB_data/U5-p49-Subject_verb_agreement.gift");
-        myMap.set(22, "./SujetB_data/U5-p52-Reading-The_death_of_cooking.gift");
-        myMap.set(23, "./SujetB_data/U5-p54-6-Passive.gift");
-        myMap.set(24, "./SujetB_data/U5-p54-GR4-Passive-reporting.gift");
-        myMap.set(25, "./SujetB_data/U5-p57-Review.gift");
-        myMap.set(26, "./SujetB_data/U6-p59-Vocabulary.gift");
-        myMap.set(27, "./SujetB_data/U6-p60-Listening.gift");
-        myMap.set(28, "./SujetB_data/U6-p61-5-Future-forms.gift");
-        myMap.set(29, "./SujetB_data/U6-p61-GR-Future_forms.gift");
-        myMap.set(30, "./SujetB_data/U6-p62_63-Reading.gift");
-        myMap.set(31, "./SujetB_data/U6-p64-Future-perfect-&-continuous.gift");
-        myMap.set(32, "./SujetB_data/U6-p65-Voc-Expressions_with_get.gift");
-        myMap.set(33, "./SujetB_data/U6-p67-Review.gift");
-        myMap.set(34, "./SujetB_data/U6-p68_69-ProgressTest2.gift");
-        myMap.set(35, "./SujetB_data/U7-p76_77-So,such,too,enough.gift");
-        myMap.set(36, "./SujetB_data/U7-p76-Relative_clauses.gift");
-        myMap.set(37, "./SujetB_data/U7-p77-6.gift");
-        myMap.set(38, "./SujetB_data/U7-p77-It is,there is.gift");
-        myMap.set(39, "./SujetB_data/U7-p79-Review-3.gift");
-        myMap.set(40, "./SujetB_data/U8-p84-Voc-Linking_words.gift");
-        myMap.set(41, "./SujetB_data/U9-p94-Listening.gift");
-        myMap.set(42, "./SujetB_data/U9-p95-Third_cond-4.gift");
-        myMap.set(43, "./SujetB_data/U10-p106-Reading.gift");
-        myMap.set(44, "./SujetB_data/U11-p114-Mixed_conditionals.gift");
-        myMap.set(45, "./SujetB_data/U4-p47-Review.gift");
-        myMap.set(46, "./SujetB_data/U5-p50-Use_of_English.gift");
+        
+        const myMap = getDataFromAllFiles();
+
         //appeler les fonctions du fichier search.js
 
         console.log("Pour placer une question dans votre fichier d'examen, vous devez spécifier un numéro de fichier, un numéro d'énoncé et un numéro de question")
@@ -403,55 +332,7 @@ program
     .command("check", 'Vérifier la qualité des données d’examen')
     .argument("<file>", "Nom du fichier à vérifier")
     .action(({args, logger}) => {
-        const myMap = new Map();
-        myMap.set(0, "./SujetB_data/EM-U4-p32_33-Review.gift");
-        myMap.set(1, "./SujetB_data/EM-U5-p34-Gra-Expressions_of_quantity.gift");
-        myMap.set(2, "./SujetB_data/EM-U5-p34-Voc.gift");
-        myMap.set(3, "./SujetB_data/EM-U5-p35-Gra-Subject_verb_agreement.gift");
-        myMap.set(4, "./SujetB_data/EM-U5-p36_37-Reading.gift");
-        myMap.set(5, "./SujetB_data/EM-U5-p38-Passive.gift");
-        myMap.set(6, "./SujetB_data/EM-U6-p46_47-4.gift");
-        myMap.set(7, "./SujetB_data/EM-U42-Ultimate.gift");
-        myMap.set(8, "./SujetB_data/U1-p7-Adverbs.gift");
-        myMap.set(9, "./SujetB_data/U1-p8_9-Reading-Coachella.gift");
-        myMap.set(10, "./SujetB_data/U1-p10-Gra-Present_tenses_habits.gift");
-        myMap.set(11, "./SujetB_data/U2-p22-Gra-Ing_or_inf.gift");
-        myMap.set(12, "./SujetB_data/U3-p30-Reading.gift");
-        myMap.set(13, "./SujetB_data/U3-p31-Gra-ed_adjectives_prepositions.gift");
-        myMap.set(14, "./SujetB_data/U3-p32-Gra-Present_perfect_simple_vs_continuous.gift");
-        myMap.set(15, "./SujetB_data/U3-p32-GR-Present_perfect_vs_past_simple.gift");
-        myMap.set(16, "./SujetB_data/U3-p33-Gra-As_like.gift");
-        myMap.set(17, "./SujetB_data/U3-p33-UoE-Hygge.gift");
-        myMap.set(18, "./SujetB_data/U4-p39-Reading-xmen.gift");
-        myMap.set(19, "./SujetB_data/U4-p42_43-Listening.gift");
-        myMap.set(20, "./SujetB_data/U5-p49-GR1-Expressions_of_quantity.gift");
-        myMap.set(21, "./SujetB_data/U5-p49-Subject_verb_agreement.gift");
-        myMap.set(22, "./SujetB_data/U5-p52-Reading-The_death_of_cooking.gift");
-        myMap.set(23, "./SujetB_data/U5-p54-6-Passive.gift");
-        myMap.set(24, "./SujetB_data/U5-p54-GR4-Passive-reporting.gift");
-        myMap.set(25, "./SujetB_data/U5-p57-Review.gift");
-        myMap.set(26, "./SujetB_data/U6-p59-Vocabulary.gift");
-        myMap.set(27, "./SujetB_data/U6-p60-Listening.gift");
-        myMap.set(28, "./SujetB_data/U6-p61-5-Future-forms.gift");
-        myMap.set(29, "./SujetB_data/U6-p61-GR-Future_forms.gift");
-        myMap.set(30, "./SujetB_data/U6-p62_63-Reading.gift");
-        myMap.set(31, "./SujetB_data/U6-p64-Future-perfect-&-continuous.gift");
-        myMap.set(32, "./SujetB_data/U6-p65-Voc-Expressions_with_get.gift");
-        myMap.set(33, "./SujetB_data/U6-p67-Review.gift");
-        myMap.set(34, "./SujetB_data/U6-p68_69-ProgressTest2.gift");
-        myMap.set(35, "./SujetB_data/U7-p76_77-So,such,too,enough.gift");
-        myMap.set(36, "./SujetB_data/U7-p76-Relative_clauses.gift");
-        myMap.set(37, "./SujetB_data/U7-p77-6.gift");
-        myMap.set(38, "./SujetB_data/U7-p77-It is,there is.gift");
-        myMap.set(39, "./SujetB_data/U7-p79-Review-3.gift");
-        myMap.set(40, "./SujetB_data/U8-p84-Voc-Linking_words.gift");
-        myMap.set(41, "./SujetB_data/U9-p94-Listening.gift");
-        myMap.set(42, "./SujetB_data/U9-p95-Third_cond-4.gift");
-        myMap.set(43, "./SujetB_data/U10-p106-Reading.gift");
-        myMap.set(44, "./SujetB_data/U11-p114-Mixed_conditionals.gift");
-        myMap.set(45, "./SujetB_data/U4-p47-Review.gift");
-        myMap.set(46, "./SujetB_data/U5-p50-Use_of_English.gift");
-
+        
         let path = args.file;
         console.log("Vous avez choisi de vérifier la qualité des données d'examen du fichier : " + args.file);
         //On recherche s'il y a des questions en double
@@ -475,14 +356,6 @@ program
             let tabLine;
             let tabCompteur;
             tabLine = data.toString().split(separator);
-
-            /*console.log("\ntab[0] = " + tabLine[0]);
-            console.log("tab[1] = " + tabLine[2]);
-            console.log("tab[2] = " + tabLine[1]);
-            console.log("La taille du tableau est de " + tabLine.length)*/
-
-            //console.log("----- Le tableau est -----");
-            //console.log(tabLine);
 
             for (let j = 0; j < tabLine.length; j++) {
                 compteur=0;
